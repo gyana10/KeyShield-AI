bbcf30a4a957a430adab146d5a53f7c3eb41add0
Gyana Ranjan Mohanty <gyanarmohanty20@gmail.com>
2026-07-09 00:41:43 +0530

    Backend Updation

--- backend/api/enrollment.py
+++ backend/api/enrollment.py
@@ -1,25 +1,69 @@
-profile_data = create_profile(data)
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from backend.db.database import get_db
+from backend.db.models import User
+from backend.db.enrollment_model import Enrollment
+from backend.db.profile_model import UserProfile
+from backend.core.dependencies import get_current_email
+from backend.ml.profile_builder import create_profile
 
-existing_profile = db.query(UserProfile).filter(
-    UserProfile.user_id == user.id
-).first()
+import json
 
-if existing_profile:
-    existing_profile.hold_mean = profile_data["hold_mean"]
-    existing_profile.hold_std = profile_data["hold_std"]
+router = APIRouter()
 
-    existing_profile.flight_mean = profile_data["flight_mean"]
-    existing_profile.flight_std = profile_data["flight_std"]
 
-    existing_profile.total_duration = profile_data["total_duration"]
-    existing_profile.backspaces = profile_data["backspaces"]
+@router.post("/enroll")
+def enroll(
+    data: dict,
+    email: str = Depends(get_current_email),
+    db: Session = Depends(get_db)
+):
 
-else:
-    profile = UserProfile(
+    user = db.query(User).filter(
+        User.email == email
+    ).first()
+
+    enrollment = Enrollment(
         user_id=user.id,
-        **profile_data
+        hold_times=json.dumps(data["holdTimes"]),
+        flight_times=json.dumps(data["flightTimes"]),
+        total_duration=data["totalDuration"],
+        backspaces=data["backspaces"]
     )
 
-    db.add(profile)
+    db.add(enrollment)
+    db.commit()
+    db.refresh(enrollment)
 
-db.commit()
+    # ---------- PROFILE CREATION ----------
+    profile_data = create_profile(data)
+
+    existing_profile = db.query(UserProfile).filter(
+        UserProfile.user_id == user.id
+    ).first()
+
+    if existing_profile:
+        existing_profile.hold_mean = profile_data["hold_mean"]
+        existing_profile.hold_std = profile_data["hold_std"]
+
+        existing_profile.flight_mean = profile_data["flight_mean"]
+        existing_profile.flight_std = profile_data["flight_std"]
+
+        existing_profile.total_duration = profile_data["total_duration"]
+        existing_profile.backspaces = profile_data["backspaces"]
+
+    else:
+        profile = UserProfile(
+            user_id=user.id,
+            **profile_data
+        )
+
+        db.add(profile)
+
+    db.commit()
+
+    return {
+        "message": "Enrollment Saved",
+        "user": user.username,
+        "id": enrollment.id
+    }
